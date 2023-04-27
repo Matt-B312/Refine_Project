de67b18a39005dfe8d5810b7bb03ed3430baa68b
Matthew Lebreton <mlebreton84813@gmail.com>
2023-04-27 11:12:02 -0400

    second commit

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -58,7 +58,7 @@ function App() {
 
       if (profileObj) {
         const response = await fetch(
-            "http://localhost:8080/api/v1/users",
+            "https://refine-project-h7bd.onrender.com/api/v1/users",
             {
                 method: "POST",
                 headers: { "Content-Type": "application/json" },
@@ -129,7 +129,7 @@ function App() {
       <GlobalStyles styles={{ html: { WebkitFontSmoothing: "auto" } }} />
       <RefineSnackbarProvider>
         <Refine
-          dataProvider={dataProvider("http://localhost:8080/api/v1")}
+          dataProvider={dataProvider("https://refine-project-h7bd.onrender.com/api/v1")}
           notificationProvider={notificationProvider}
           ReadyPage={ReadyPage}
           catchAll={<ErrorComponent />}
